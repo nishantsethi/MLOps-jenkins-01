189fa3807ea117bf76f609a1747b4162fcd3ba4e
Nishant Sethi <sethi.nishant43@gmail.com>
2021-05-02 16:02:50 +0530

    Added ML Model Tests
    Add script to be ran via Jenkins that will test for accuracy

--- tests/01.sh
+++ tests/01.sh
@@ -1,2 +1,25 @@
-python3 /appl1/iris/script/test.py
-cat /appl1/iris/script/accuracy.txt
+#!/bin/bash
+
+
+LASTRECORD="$WORKSPACE/tests/last_result.txt"
+ACCURACYFILE="$WORKSPACE/script/accuracy.txt"
+SCORE=`cat $ACCURACYFILE`
+
+
+if [ ! -e "$LASTRECORD" ] ; then
+    echo 0 > "$LASTRECORD"
+fi
+
+LASTSCORE=`cat $LASTRECORD`
+
+if [ ! $SCORE -ge $LASTRECORD ] ; then
+    echo "New Accuracy($SCORE) is lower than Previous accuracy($LASTRECORD)"
+    echo "Test 1 Failed as Accuracy is not above the Last recorded accuracy"
+    echo "Exiting Script"
+    exit 42
+else
+    echo "New Accuracy($SCORE) is greater or equal to the Previous accuracy($LASTRECORD)"
+    echo "Test 1 Passed"
+
+echo "All Tests Passed"
+echo "Script Termed OK"
